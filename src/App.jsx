6f948116e84fc40bf4d3ea7672520b3eedb82351
pandacover <luvmakin01@gmail.com>
2022-05-17 16:55:16 +0530

    Bootstrapped with cra / installed tailwind / edited for simplicity

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div className='bg-black container text-white mx-auto h-screen'>
+          <p>Hello World</p>
+        </div>
+    )
+}
+
+export default App;
